99e945cc6413c3edfa92815f654124f0e80f846d
Lan-c <stataf.dev@gmail.com>
2025-08-20 13:01:53 +0100

    added some new things

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-const QA = {
-  q: "What is the capital of Palestine ?",
-  a: ["Jerusalem", "Ramallah", "Gaza", "Deir el Balah"],
-  cIdx: 0,
-};
+const QA = [
+  {
+    q: "What is the capital of Palestine ?",
+    a: ["Jerusalem", "Ramallah", "Gaza", "Deir el Balah"],
+    cIdx: 0,
+  },
+  {
+    q: "Chkoun el Zaeim ?",
+    a: ["Mus", "Djawad", "Anis", "Zaeim"],
+    cIdx: 0,
+  },
+  {
+    q: "Who was in Paris ?",
+    a: ["Kanye", "Merouane", "Anouche Mafia", "Niggas"],
+    cIdx: 3,
+  },
+];
 
+let qidx = 0;
 let selectedAnswer = null;
+let hasConfirmed = false;
+let time = 4;
+let timerFunction;
 
 const gameSection = document.getElementById("game");
 const startSection = document.getElementById("start");
+const timerSection = document.getElementById("timer");
 const qtext = document.getElementById("question-text");
 const optionsList = document.getElementById("options-list");
+const nextButton = document.querySelector(".next");
+const timerButton = document.getElementById("timer-btn");
 
 const start = () => {
   startSection.style.display = "none";
-  gameSection.style.display = "flex";
-  qtext.textContent = QA.q;
+  timerSection.style.display = "flex";
+  timer();
+};
 
-  const answers = QA.a
+const timer = () => {
+  timerFunction = setInterval(() => {
+    timerButton.textContent = time;
+    if (time == 0) {
+      timerButton.textContent = "START";
+      timerButton.addEventListener("click", startQuiz);
+      clearInterval(timerFunction);
+    }
+    time--;
+  }, 1000);
+};
+
+const startQuiz = () => {
+  timerSection.style.display = "none";
+  gameSection.style.display = "flex";
+  qtext.textContent = QA[qidx].q;
+
+  const answers = QA[qidx].a
     .map((el, idx) => {
       return `<li class="option" onclick="select(${idx})">${el}</li>`;
     })
@@ -31,7 +68,9 @@ const select = (idx) => {
 
   options.forEach((el, index) => {
     el.classList.remove("selected");
-    if (index == idx) el.classList.add("selected");
+    if (index == idx && !hasConfirmed) {
+      el.classList.add("selected");
+    }
   });
 };
 
@@ -41,13 +80,34 @@ const confirm = () => {
     return;
   }
 
-  const newAnswers = QA.a
+  if (hasConfirmed) return;
+
+  const newAnswers = QA[qidx].a
     .map((el, idx) => {
       const correctClassname =
-        idx == QA.cIdx ? "correct" : idx == selectedAnswer ? "incorrect" : "";
+        idx == QA[qidx].cIdx
+          ? "correct"
+          : idx == selectedAnswer
+          ? "incorrect"
+          : "";
       return `<li class="option ${correctClassname}" data-index="${idx}" onclick="select(${idx})">${el}</li>`;
     })
     .join("");
 
   optionsList.innerHTML = newAnswers;
+  hasConfirmed = true;
+  nextButton.style.display = "block";
+};
+
+next = () => {
+  if (!hasConfirmed) return;
+  if (qidx < QA.length - 1) {
+    qidx++;
+    console.log("from next question : ", qidx);
+    hasConfirmed = false;
+    nextButton.style.display = "none";
+    startQuiz();
+  } else {
+    window.alert("Quiz finished");
+  }
 };
